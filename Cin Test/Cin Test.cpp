bded072497732755fcece2ced8bf23694f943f6e
Rosaline Flowers <59781247+SereneMOS@users.noreply.github.com>
2022-09-27 18:09:47 -0400

    Add files via upload

---
+++ Cin Test/Cin Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a number: ";
+    int x{};
+    std::cin >> x;
+    std::cout << "You entered " << x << '\n';
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
